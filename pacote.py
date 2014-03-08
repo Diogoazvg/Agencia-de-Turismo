74c5657d311d1dae76ef03e477cd15090d3a8095
DiogoAzvg <diogo.azvg@gmail.com>
2014-03-08 18:36:00 -0300

    Classe pacote

---
+++ pacote.py
@@ -0,0 +1,8 @@
+class Pacote:
+    def __init__ (self,codPacote,codTransporte=[None],codHotel=[None],codRefeicao=[None],pessoa=[None]):
+        self.codPacote = codPacote
+        self.codTransporte = codTransporte
+        self.codHotel = codHotel
+        self.codRefeicao = codRefeicao
+        self.pessoa = pessoa
+
