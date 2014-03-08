7cad896439050c1700a410a590040c86164bdb51
DiogoAzvg <diogo.azvg@gmail.com>
2014-03-08 18:12:49 -0300

    teste de transporte

---
+++ spec/transporte_spec.py
@@ -0,0 +1,15 @@
+import unittest
+from should_dsl import should
+from transporte import Transporte
+
+class Transporte_spec(unittest.TestCase):
+
+    def it_creates_um_object_transporte(self):
+        transporte = Transporte ('001','1001','www.1001.com.br','27338980','Rio','Bahia','R$ 3000')
+        transporte.codTransporte |should| equal_to ('001')
+        transporte.cia |should| equal_to ('1001')
+        transporte.site |should| equal_to ('www.1001.com.br')
+        transporte.telefone |should| equal_to ('27338980')
+        transporte.origem |should| equal_to ('Rio')
+        transporte.destino |should| equal_to ('Bahia')
+        transporte.precoViagem |should| equal_to ('R$ 3000')
