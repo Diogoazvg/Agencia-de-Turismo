9f100e04b2c6eddaceff3c46fb10406394d5d06d
DiogoAzvg <diogo.azvg@gmail.com>
2014-03-08 18:00:28 -0300

    teste de pessoa

--- spec/pessoa_spec.py
+++ spec/pessoa_spec.py
@@ -6,4 +6,9 @@ class Pessoa_spec(unittest.TestCase):
 
     def it_creates_a_pessoa_object(self):
         pessoa = Pessoa ('001','Diogo','3','998845108','diogo.azvg@gmail.com')
+        pessoa.codPessoa |should| equal_to ('001')
+        pessoa.cliente |should| equal_to ('Diogo')
+        pessoa.quantDependente |should| equal_to ('3')
+        pessoa.telefone |should| equal_to ('998845108')
+        pessoa.email |should| equal_to ('diogo.azvg@gmail.com')
 
