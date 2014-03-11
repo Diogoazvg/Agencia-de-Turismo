d272dc1e176487af59ce7075dfc8b237bd58ea23
Clovis Junior <clovisrangeljr@gmail.com>
2014-03-11 17:09:21 -0300

    Teste de update em Transporte

--- spec/transporte_spec.py
+++ spec/transporte_spec.py
@@ -13,3 +13,10 @@ class Transporte_spec(unittest.TestCase):
         transporte.origem |should| equal_to ('Rio')
         transporte.destino |should| equal_to ('Bahia')
         transporte.precoViagem |should| equal_to ('R$ 3000')
+
+    def it_update(self):
+    	transporte = Transporte ('001','1001','www.1001.com.br','27338980','Rio','Bahia','R$ 3000')
+    	transporte.update('Itapemirim', 'www.Itapemirim.com.br', 'R$ 2200')
+    	transporte.cia |should| equal_to('Itapemirim')
+    	transporte.site |should| equal_to('www.Itapemirim.com.br')
+    	transporte.precoViagem |should| equal_to('R$ 2200')    
